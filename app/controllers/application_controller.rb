44faa1feca0707eaa1e310eaaaacaa0d9a60b99c
prototypefilverse <morikawasusumu.b@gmail.com>
2023-01-01 19:04:33 +0900

    ユーザーの新規登録ができる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :family_name, :first_name_kana, :family_name_kana,  :birth_day])
   end
 
 end
