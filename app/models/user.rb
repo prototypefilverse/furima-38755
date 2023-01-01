0b58291db6c1d215eac14dc0e52a7f20d5d34543
prototypefilverse <morikawasusumu.b@gmail.com>
2023-01-01 23:59:00 +0900

    ログイン実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX }       
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'Include both letters and numbers' }       
 
   validates :nickname,         presence: true
   validates :first_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
