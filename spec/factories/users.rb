380a28ab7783a13af7be3c94c80a3abb64bc3bfe
prototypefilverse <morikawasusumu.b@gmail.com>
2023-01-03 18:40:56 +0900

    nicknameが空では登録できない

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {'sample'}
     email                 {'sample@gmail.com'}
-    password              {'000000'}
+    password              {'aaa000'}
     password_confirmation {password}
     first_name            {'太朗'}
     family_name           {'山田'}
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
 
+  describe 'ユーザー新規登録' do
+    
+    it 'nicknameが空では登録できない' do
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Nickname can't be blank"
+    end
+
+  end
 
 end
