fe2af6d9e508b26b7c7688091077f1cc09a6c7b4
prototypefilverse <morikawasusumu.b@gmail.com>
2023-01-04 20:44:29 +0900

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'Include both letters and numbers' }       
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'Include both letters and numbers' }
 
   validates :nickname,         presence: true
   validates :first_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
@@ -13,6 +13,4 @@ class User < ApplicationRecord
   validates :first_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_day,        presence: true
-
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'sample'}
-    email                 {Faker::Internet.free_email}
-    password              {'aaa000'}
-    password_confirmation {password}
-    first_name            {'太朗'}
-    family_name           {'山田'}
-    first_name_kana       {'タロウ'}
-    family_name_kana      {'ヤマダ'}
-    birth_day             {'2000-01-01'}
+    nickname              { 'sample' }
+    email                 { Faker::Internet.free_email }
+    password              { 'aaa000' }
+    password_confirmation { password }
+    first_name            { '太朗' }
+    family_name           { '山田' }
+    first_name_kana       { 'タロウ' }
+    family_name_kana      { 'ヤマダ' }
+    birth_day             { '2000-01-01' }
   end
 end
