d2bee3d9b2e57b6030ef47a4ea386ec9413861e3
prototypefilverse <morikawasusumu.b@gmail.com>
2023-01-06 14:23:01 +0900

    一旦バリデとアソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+
+  validates :user_id,          presence: true
+  validates :name,             presence: true
+  validates :description,      presence: true
+  validates :category_id,      presence: true
+  validates :status_id,        presence: true
+  validates :shipping_cost_id, presence: true
+  validates :prefecture_id,    presence: true
+  validates :shipping_day_id,  presence: true
+  validates :price,            presence: true
+  validates :image,            presence: true
+
+  belongs_to :user
+  has_one    :order
+
+  has_one_attached :image
+
 end
