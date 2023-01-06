f55e48a858f26e63de6bfe9e278046affa60c7aa
prototypefilverse <morikawasusumu.b@gmail.com>
2023-01-06 19:00:03 +0900

    バリデーション記述の完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,19 +15,16 @@ class Item < ApplicationRecord
   
   validates :name,             presence: true
   validates :description,      presence: true
-  validates :category_id,      presence: true
-  validates :status_id,        presence: true
-  validates :shipping_cost_id, presence: true
-  validates :prefecture_id,    presence: true
-  validates :shipping_day_id,  presence: true
-  validates :price,            presence: true
+  validates :category_id,      presence: true, numericality: { other_than: 0 , message: "can't be blank" } 
+  validates :status_id,        presence: true, numericality: { other_than: 0 , message: "can't be blank" } 
+  validates :shipping_cost_id, presence: true, numericality: { other_than: 0 , message: "can't be blank" } 
+  validates :prefecture_id,    presence: true, numericality: { other_than: 0 , message: "can't be blank" } 
+  validates :shipping_day_id,  presence: true, numericality: { other_than: 0 , message: "can't be blank" } 
+  validates :price,            presence: true, 
+                           numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, 
+                                message: { "out of range" },
+                                 format: { with: /\A[0-9]+\z/ }
   validates :image,            presence: true
-
-  validates :category_id,      numericality: { other_than: 0 , message: "can't be blank" } 
-  validates :status_id,        numericality: { other_than: 0 , message: "can't be blank" } 
-  validates :shipping_cost_id, numericality: { other_than: 0 , message: "can't be blank" } 
-  validates :prefecture_id,    numericality: { other_than: 0 , message: "can't be blank" } 
-  validates :shipping_day_id,  numericality: { other_than: 0 , message: "can't be blank" } 
 
 
 end
