e65082cf6daa9bfbf4c527f0cb36fac37a8fa1ab
prototypefilverse <morikawasusumu.b@gmail.com>
2023-01-08 17:27:08 +0900

    テストコード準備と二つ実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+
+    name             { 'sample' }
+    description      { 'sample' }
+    category_id      { 1 }
+    status_id        { 1 }
+    shipping_cost_id { 1 }
+    prefecture_id    { 1 }
+    shipping_day_id  { 1 }
+    price            { 1000 }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.create(:item)
+  end
+
+  describe '商品の出品' do
+
+    context '出品できるとき' do
+
+      it '全ての項目が正しく入力されていれば出品できる' do
+        expect(@item).to be_valid
+      end
+
+    end
+
+    context '出品できないとき' do
+ 
+      it '商品名が空では出品できない' do
+        @item.name = " "
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it '商品の説明が空では出品できない' do
+        @item.description = " "
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+
+
+    end
+
+  end
+
 end
