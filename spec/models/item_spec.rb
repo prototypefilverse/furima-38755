99c399615baf873062d5ef944b050dc5ab706ea1
prototypefilverse <morikawasusumu.b@gmail.com>
2023-01-09 16:26:27 +0900

    buildに修正、user紐付けテストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before do
-    @item = FactoryBot.create(:item)
+    @item = FactoryBot.build(:item)
     sleep(0.1)
   end
 
@@ -14,6 +14,13 @@ RSpec.describe Item, type: :model do
     end
 
     context '出品できないとき' do
+
+      it "userが紐づいていないと出品できない" do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+
       it '商品画像を1枚つけないと出品できない' do
         @item.image = nil
         @item.valid?
