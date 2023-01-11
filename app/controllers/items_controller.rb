f5bbe46f07683a838bb9a41d3890f920617ebfd4
prototypefilverse <morikawasusumu.b@gmail.com>
2023-01-11 17:52:48 +0900

    ログイン状態の場合にのみ、自身が出品した商品情報を削除できる。処理をまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
 
   def index
     @items = Item.all.includes(:user).order('created_at DESC')
@@ -36,9 +36,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
+    if current_user.id == @item.user_id
     @item.destroy
     redirect_to root_path
+    else
+    redirect_to root_path
+    end   
   end  
 
   private
