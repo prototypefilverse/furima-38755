694abd4179338bd853f6b54835028bd32a8dbf74
prototypefilverse <morikawasusumu.b@gmail.com>
2023-01-15 16:03:46 +0900

    payment側のモデルからの記述をした

--- app/models/order_payment.rb
+++ app/models/order_payment.rb
@@ -1,6 +1,18 @@
 class OrderPayment
   include ActiveModel::Model
 
+  attr_accessor :order_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number
 
+  with_options presence: true do
+    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :city
+    validates :block
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "not correct" }
+  end
+
+  def save    
+    Payment.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, order_id: order.id )
+  end
 
 end
