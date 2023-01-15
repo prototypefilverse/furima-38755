7687b419e2b261d1ad0166c3f840b56ad38ab8a7
prototypefilverse <morikawasusumu.b@gmail.com>
2023-01-16 00:22:49 +0900

    購入するを押したらデータベースに登録されトップに戻る。入力不備でエラーを表示する。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,4 +5,22 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
+  def create
+    @order_payment = OrderPayment.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order_payment.valid?
+      @order_payment.save
+      redirect_to root_path
+    else
+      render :index
+    end
+
+  end
+
+  private
+
+  def order_params
+    params.require(:order_payment).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
