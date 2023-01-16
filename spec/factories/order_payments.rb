57adfc2d6f500f8e1eeafb276a9afe41dee9f0db
prototypefilverse <morikawasusumu.b@gmail.com>
2023-01-16 15:00:52 +0900

    ファクトリーボット記述

--- spec/factories/order_payments.rb
+++ spec/factories/order_payments.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
-  factory :order_payment do
+      factory :order_payment do
+        postcode      { 111-1111 }
+        prefecture_id { 1 }
+        city          { "大阪市" }
+        block         { "大阪6-5" }
+        building      { "大阪ビル" }
+        phone_number  { 11111111111 }
     
   end
 end
